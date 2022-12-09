0c5ad71897f7fe5a9c55517a319c21ce75a0aeac
Apbruen <113388150+Apbruen@users.noreply.github.com>
2022-12-09 15:45:15 -0500

    Add files via upload

---
+++ Hello_World_Bruen/README.md
@@ -0,0 +1,1 @@
+# Bruen_Lab11
---
+++ Hello_World_Bruen/src/com/hello_world_bruen/Hello_World_Bruen.java
@@ -0,0 +1,9 @@
+package com.hello_world_bruen;
+
+public class Hello_World_Bruen {
+	
+	public static void main(String args[]) {
+		System.out.print("Hello World, this is Aidan!");
+	}
+
+}
